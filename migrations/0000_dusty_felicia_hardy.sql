d04013d0f444897024e4f1cfe4fa7db644df0fa9
Faizuddin Mohammed <faiz@konic.net>
2025-05-03 09:57:59 +0530

    chore: schema + migrations

--- migrations/0000_dusty_felicia_hardy.sql
+++ migrations/0000_dusty_felicia_hardy.sql
@@ -1,18 +1,28 @@
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`email` text NOT NULL,
+	`created_at` integer NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
+--> statement-breakpoint
 CREATE TABLE `api_keys` (
 	`id` integer PRIMARY KEY NOT NULL,
 	`key` text NOT NULL,
-	`created_at` integer DEFAULT now() NOT NULL,
+	`created_at` integer NOT NULL,
 	`user_id` integer NOT NULL,
-	`last_used` integer DEFAULT now() NOT NULL,
+	`last_used` integer NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
-CREATE UNIQUE INDEX `api_keys_key_unique` ON `api_keys` (`key`);--> statement-breakpoint
+CREATE UNIQUE INDEX `api_keys_key_unique` ON `api_keys` (`key`);
+--> statement-breakpoint
 CREATE TABLE `bots` (
 	`id` integer PRIMARY KEY NOT NULL,
 	`name` text NOT NULL,
 	`description` text,
-	`created_at` integer DEFAULT now() NOT NULL,
+	`created_at` integer NOT NULL,
 	`user_id` integer NOT NULL,
 	`picture` text,
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
@@ -21,17 +31,8 @@ CREATE TABLE `bots` (
 CREATE TABLE `messages` (
 	`id` integer PRIMARY KEY NOT NULL,
 	`content` text NOT NULL,
-	`created_at` integer DEFAULT now() NOT NULL,
+	`created_at` integer NOT NULL,
 	`bot_id` integer NOT NULL,
 	`role` text NOT NULL,
 	FOREIGN KEY (`bot_id`) REFERENCES `bots`(`id`) ON UPDATE no action ON DELETE no action
 );
---> statement-breakpoint
-CREATE TABLE `users` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`name` text NOT NULL,
-	`email` text NOT NULL,
-	`created_at` integer DEFAULT now() NOT NULL
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,15 @@ app.get("/", async (c) => {
   return c.json({ message: "Hello Hono!", users: result });
 });
 
+app.get("/users", async (c) => {
+  const db = drizzle(c.env.DB);
+
+  // Example query using Drizzle ORM
+  const result = await db.select().from(users).all();
+
+  return c.json({ users: result });
+});
+
 // Example route for creating a user
 app.post("/users", async (c) => {
   const db = drizzle(c.env.DB);
